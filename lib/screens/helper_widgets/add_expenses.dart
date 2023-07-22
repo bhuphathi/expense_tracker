2ccbb659acc250f6f0dd83a3ce0096cda35f3cda
bhuphathi <bhuphathi@gmail.com>
2023-07-22 15:30:45 +0530

    feat: amount textfield and cancel btn added

--- lib/screens/helper_widgets/add_expenses.dart
+++ lib/screens/helper_widgets/add_expenses.dart
@@ -9,10 +9,12 @@ class AddExpenses extends StatefulWidget {
 
 class _AddExpenses extends State<AddExpenses> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -22,19 +24,35 @@ class _AddExpenses extends State<AddExpenses> {
       padding: const EdgeInsets.all(16),
       child: Column(children: [
         TextField(
-          maxLength: 50,
           controller: _titleController,
           decoration: const InputDecoration(
             label: Text("Title"),
           ),
         ),
+        TextField(
+          controller: _amountController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            prefixText: "Rs.",
+            label: Text("Amount"),
+          ),
+        ),
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
             ElevatedButton(
-                onPressed: () {
-                  print(_titleController);
-                },
-                child: const Text("Save Changes"))
+              onPressed: () {
+                print(_titleController);
+                print(_amountController);
+              },
+              child: const Text("Save Changes"),
+            ),
           ],
         )
       ]),
